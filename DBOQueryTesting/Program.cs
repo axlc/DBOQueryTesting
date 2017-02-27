3173f50011591590955cdf076579cf3831872ba9
Axl M. <axl.mccracken@gmail.com>
2017-02-27 12:46:15 -0600

    Add project files.

---
+++ DBOQueryTesting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using A2_Reg_A_Lot_2017;
+
+namespace DBOQueryTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBOQuery insertNewUser = new DBOQuery();
+            int newRowNumber = 0;
+
+            // insertNewUser.InsertIntoUsers("bob", 1);
+            //insertNewUser.InsertIntoContactDetails(1, "Axl", "Mcc",
+            //                                       "", "", "",
+            //                                       "", "", "",
+            //                                       "", "");
+            string courseDescription = "Testing Course Description.";
+            newRowNumber = insertNewUser.InsertIntoCourses("Test Course 1", 
+                                                           "TEST-0001", "007", courseDescription,
+                                                           "1:13", "2:00", 3.51);
+            Console.WriteLine("Data was inserted to row: {0}", newRowNumber);
+            Console.ReadKey();
+            
+        }
+    }
+}
